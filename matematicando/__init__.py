b50b41fbef3ba9184e532460f4fd3b9f242c4a7f
fiixed <null>
2023-04-02 20:15:05 -0300

    importing blueprints and registering them

--- matematicando/__init__.py
+++ matematicando/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
-# from .views home_bp, materials_bp
+from .views import home_bp
 
 
 def page_not_found(e):
     render_template("404.html"), 404
 
 
+def _register_blueprints(app: Flask):
+    app.register_blueprint(home_bp.home_bp, url_prefix="/")
+
+
 def create_app():
     app = Flask(__name__)
     app.register_error_handler(404, page_not_found)
+    _register_blueprints(app)
     return app

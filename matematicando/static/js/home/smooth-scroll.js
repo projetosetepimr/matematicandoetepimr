db74f6a7a473e7644118483f4b418ce03e9c3b66
fiixed <null>
2023-04-05 20:03:36 -0300

    basic footer and smooth scrolling to information section

---
+++ matematicando/static/js/home/smooth-scroll.js
@@ -0,0 +1,35 @@
+const exploreButton = document.getElementById("explore-button")
+const scrollTo = (document.getElementById("information").offsetTop)
+
+exploreButton.addEventListener("click", scrollToId)
+
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset
+  const startY = window.scrollY || window.pageYOffset
+  const distanceX = endX - startX
+  const distanceY = endY - startY
+  const startTime = new Date().getTime()
+
+  duration = typeof duration !== 'undefined' ? duration : 400
+
+  // Easing function
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from
+  }
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime
+    const newX = easeInOutQuart(time, startX, distanceX, duration)
+    const newY = easeInOutQuart(time, startY, distanceY, duration)
+    if (time >= duration) {
+      clearInterval(timer)
+    }
+    window.scroll(newX, newY)
+  }, 1000 / 60)
+}
+
+function scrollToId() {
+	// console.log(scrollTo)
+	smoothScrollTo(0, scrollTo, 800)
+}

1613c8d5ca34e19ed4851d4c70b48ad98d878e10
fiixed <null>
2023-04-24 21:08:18 -0300

    gunicorn config file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,8 +3,8 @@ from os import getenv
 
 bind = f"0.0.0.0:{getenv('PORT')}"
 workers = 5
-accesslogfile = "-"
-errorlogfile = "-"
-loglevel = "debug"
-access_logf_ormat = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s "%(a)s"'
 max_requests = 1000
